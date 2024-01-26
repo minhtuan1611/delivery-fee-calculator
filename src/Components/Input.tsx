a0ecbf8fabc3c6876949ace6ea16aa98d2514b53
minhtuan1611 <tuan.mn51@gmail.com>
2024-01-26 10:11:06 +0700

    commit_26_1

--- src/Components/Input.tsx
+++ src/Components/Input.tsx
@@ -7,85 +7,55 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 
 interface InputProps {
+  inputValues: InputValues
+  handleChange: (
+    event: ChangeEvent<HTMLInputElement>,
+    inputName: keyof InputValues
+  ) => void
+}
+interface InputValues {
   cartValue: number
   distance: number
   numberOfItems: number
   orderTime: Date
-  handleChange: (
-    event: ChangeEvent<HTMLInputElement>,
-    inputName: string
-  ) => void
 }
 
-const Input: React.FC<InputProps> = ({
-  cartValue,
-  distance,
-  numberOfItems,
-  orderTime,
-  handleChange,
-}) => (
+const Input: React.FC<InputProps> = ({ inputValues, handleChange }) => (
   <div className="input-container">
     <div className="input-section">
-      <div className="input-row">
-        <label className="input-label" htmlFor="cartValue">
-          Cart Value:
-        </label>
-        <input
-          className="input-field"
-          type="text"
-          id="cartValue"
-          data-test-id="cartValue"
-          value={cartValue}
-          onChange={(e) => handleChange(e, 'cartValue')}
-        />
-        <FontAwesomeIcon className="input-icon" icon={faEuroSign} />
-      </div>
-
-      <div className="input-row">
-        <label className="input-label" htmlFor="distance">
-          Delivery Distance:
-        </label>
-        <input
-          className="input-field"
-          type="text"
-          id="distance"
-          data-test-id="distance"
-          value={distance}
-          onChange={(e) => handleChange(e, 'distance')}
-        />
-        <FontAwesomeIcon icon={faTruckFast} className="input-icon" />
-      </div>
-    </div>
-
-    <div className="input-section">
-      <div className="input-row">
-        <label className="input-label" htmlFor="numberOfItems">
-          Amount of Items:
-        </label>
-        <input
-          className="input-field"
-          type="text"
-          id="numberOfItems"
-          data-test-id="numberOfItems"
-          value={numberOfItems}
-          onChange={(e) => handleChange(e, 'numberOfItems')}
-        />
-        <FontAwesomeIcon icon={faCartFlatbed} className="input-icon" />
-      </div>
-
-      <div className="input-row">
-        <label className="input-label" htmlFor="orderTime">
-          Order Time:
-        </label>
-        <input
-          className="input-field-time"
-          type="datetime-local"
-          id="orderTime"
-          data-test-id="orderTime"
-          value={orderTime.toISOString().slice(0, 16)}
-          onChange={(e) => handleChange(e, 'orderTime')}
-        />
-      </div>
+      {Object.entries(inputValues).map(([name, value]) => (
+        <div className="input-row" key={name}>
+          <label className="input-label" htmlFor={name}>
+            {name === 'orderTime'
+              ? 'Order Time'
+              : ` ${name.charAt(0).toUpperCase() + name.slice(1)}`}
+            :
+          </label>
+          <input
+            className={`input-field${name === 'orderTime' ? '-time' : ''}`}
+            type={name === 'orderTime' ? 'datetime-local' : 'number'}
+            required
+            id={name}
+            data-test-id={name}
+            value={
+              name === 'orderTime' ? value.toISOString().slice(0, 16) : value
+            }
+            onChange={(e) => handleChange(e, name as keyof InputValues)}
+          />
+          {name !== 'orderTime' && (
+            <FontAwesomeIcon
+              className="input-icon"
+              icon={
+                name === 'cartValue'
+                  ? faEuroSign
+                  : name === 'distance'
+                  ? faTruckFast
+                  : faCartFlatbed
+              }
+            />
+          )}
+        </div>
+      ))}
     </div>
   </div>
 )
