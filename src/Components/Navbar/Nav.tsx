4dc10992e8c59577fc9b1cc772cd8c7bbc4d855a
minhtuan1611 <tuan.mn51@gmail.com>
2024-01-30 03:18:06 +0700

    commit_30_1_2

--- src/Components/Navbar/Nav.tsx
+++ src/Components/Navbar/Nav.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import '../../App.css'
 import * as data from './links.json'
 import logo from '../../Asset/Image/logo.png'
+import { NavBar } from '../../styles'
 
 const linksString = JSON.stringify(data)
 const links = JSON.parse(linksString).links
@@ -25,14 +26,14 @@ const Links: React.FC<{ links: Link[] }> = ({ links }) => {
 
 const Nav: React.FC<{}> = () => {
   return (
-    <nav className="navbar">
+    <NavBar>
       <div className="logo-container">
         <a href="https://wolt.com/en">
           <img src={logo} alt="Logo" />
         </a>
       </div>
       <Links links={links} />
-    </nav>
+    </NavBar>
   )
 }
 
--- src/styles.tsx
+++ src/styles.tsx
@@ -126,3 +126,13 @@ export const FeeText = styled.p`
   font-size: 16px;
   color: var(body_color);
 `
+
+export const NavBar = styled.nav`
+  width: 100%;
+  height: 50px;
+  background: #fff;
+  border-bottom: 1px solid #555;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
